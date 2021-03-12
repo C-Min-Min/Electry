30121e367841736ac91b27165081a04561f71815
KokosTech <kokolia1070@gmail.com>
2021-03-12 16:20:08 +0200

    [INIT] Dashboard components

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import props from 'prop-types';
 
 const Dashboard = ({title}) => {
-  title("Dashboard"); 
+  title = "Dashboard"; 
   return(
     <div>
       <h3>Smth</h3>
