f74b51fbbdcfeb4017c14d990f94f172048c4ef1
Kaloyan Doychonov <kokolia1070@gmail.com>
2021-03-14 02:19:38 +0200

    [FIX] Connection

--- web_app/node-mysql/connection.js
+++ web_app/node-mysql/connection.js
@@ -1,4 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+
 let mysql = require('mysql');
+const { element } = require('prop-types');
+
 
 let connection = mysql.createConnection({
     host: '34.107.85.119',
@@ -7,73 +13,26 @@ let connection = mysql.createConnection({
     database: 'electry'
 });
 
-let tables = []
+let tables = [];
 
 connection.connect(function(err) {
     if (err) throw err;
-    connection.query("SHOW TABLES FROM `electry` LIKE '%measure%'", function (err, result, fields) {
+
+    connection.query("SELECT table_name FROM information_schema.TABLES WHERE table_name LIKE '%measure%'order by CREATE_TIME desc limit 1", function (err, result, fields) {
       if (err) throw err;
       
       for(var i of result){
         for(var j of Object.values(i)){
-          connection.query("SELECT * FROM `" + j +"`", function (err, result_2, fields) {
+          connection.query("SELECT * FROM `" + j +"` limit 24", function (err, result, fields) {
             if (err) throw err
-            for(var z of result_2){
-              for(var x of Object.values(z)){
-                tables.push(x)
-              }
+            for(var z of result){
+                tables.push(z)
+
             }
+            let data = JSON.stringify(tables);
+            fs.appendFileSync('tables.json', data);
           });
         }
       }
     });
-    console.log("Nice!")
-    console.log(tables)
 });
-
-
-
-/* let mysql = require('mysql');
-
-let connection = mysql.createConnection({
-    host: '34.107.85.119',
-    user: 'root',
-    password: 'hacktues_c',
-    database: 'electry'
-});
-
-let tables = []
-let isReady = false
-
-const conn = () => {
-  return new Promise(resolve => {
-    connection.connect(function(err) {
-      if (err) throw err;
-        connection.query("SHOW TABLES FROM `electry` LIKE '%measure%'", function (err, result, fields) {
-          if (err) throw err;
-          
-          for(var i of result){
-            for(var j of Object.values(i)){
-              connection.query("SELECT * FROM `" + j +"`", function (err, result_2, fields) {
-                if (err) throw err
-                for(var z of result_2){
-                  for(var x of Object.values(z)){
-                    tables.push(x)
-                  }
-                }
-              });
-            }
-          }
-          
-        });
-  
-    });console.log(tables)
-  });
-}
-
-async function  check(){
-  await conn()
-  console.log(isReady)
-} 
-
-check() */
